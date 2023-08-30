0a1af7c81f4e9ea9c6eb23383d9045cb9f5e0364
adaxiik <a.jantula@seznam.cz>
2023-08-30 22:13:54 +0200

    vscode bindings and autocomplete

--- src/wasm/editor.js
+++ src/wasm/editor.js
@@ -3,11 +3,15 @@ function init_editor(editor_id)
     let editor = ace.edit(editor_id);
     editor.setTheme("ace/theme/monokai");
     editor.session.setMode("ace/mode/c_cpp");
+    editor.setKeyboardHandler("ace/keyboard/vscode");
     editor.setValue("int a = 10;", 1); //1 = moves cursor to end
     editor.clearSelection();
     editor.focus();
     editor.setOptions({
-        fontSize: "16pt"
+        fontSize: "16pt",
+        enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: true
     });
 
     return editor;
