aeb941e2032c139d4bde642377d01a75eea84005
adaxiik <a.jantula@seznam.cz>
2023-09-17 20:25:12 +0200

    clean up

--- src/include/scc/interpreter.hpp
+++ src/include/scc/interpreter.hpp
@@ -26,32 +26,7 @@ namespace scc
         InterpreterState m_state;
         std::vector<lowering::Instruction> m_program;
         
-
         InterpreterResult register_functions(binding::BoundBlockStatement &block_statement);
-
-        // InterpreterResult interpret(const binding::BoundBlockStatement &block_statement);
-        // InterpreterResult interpret(const binding::BoundStatement &statement);
-        // InterpreterResult interpret(const binding::BoundVariableDeclarationStatement &variable_declaration);
-        // InterpreterResult interpret(const binding::BoundVariableValueDeclarationStatement &variable_value_declaration);
-        // InterpreterResult interpret(const binding::BoundVariablePointerDeclarationStatement &variable_pointer_declaration);
-        // InterpreterResult interpret(const binding::BoundVariableStaticArrayDeclarationStatement &variable_reference_declaration);
-        // InterpreterResult interpret(const binding::BoundIfStatement &if_statement);
-        // InterpreterResult interpret(const binding::BoundWhileStatement &while_statement);
-        // InterpreterResult interpret(const binding::BoundDoStatement &do_statement);
-        // InterpreterResult interpret(const binding::BoundBreakStatement &break_statement);
-        // InterpreterResult interpret(const binding::BoundContinueStatement &continue_statement);
-        // InterpreterResult interpret(const binding::BoundFunctionStatement &function);
-        // InterpreterResult interpret(const binding::BoundReturnStatement &return_statement);
-
-        // InterpreterResult eval(const binding::BoundExpression &expression);
-        // InterpreterResult eval(const binding::BoundBinaryExpression &binary_expression);
-        // InterpreterResult eval(const binding::BoundCastExpression &cast_expression);
-        // InterpreterResult eval(const binding::BoundParenthesizedExpression &parenthesized_expression);
-        // InterpreterResult eval(const binding::BoundIdentifierExpression &identifier_expression);
-        // InterpreterResult eval(const binding::BoundAssignmentExpression &assignment_expression);
-        // InterpreterResult eval(const binding::BoundLiteralExpression &literal_expression);
-        // InterpreterResult eval(const binding::BoundCallExpression &call_expression);
-        
 
     };
 
