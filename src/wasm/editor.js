7391e785b0f7644e26c1b50055e8635486237cad
adaxiik <a.jantula@seznam.cz>
2023-10-22 19:51:09 +0200

    ok, I blame myself

--- src/wasm/editor.js
+++ src/wasm/editor.js
@@ -4,7 +4,25 @@ function init_editor(editor_id)
     editor.setTheme("ace/theme/monokai");
     editor.session.setMode("ace/mode/c_cpp");
     editor.setKeyboardHandler("ace/keyboard/vscode");
-    editor.setValue("int a = 10;", 1); //1 = moves cursor to end
+    editor.setValue(`#include <stdio.h>
+
+void _scc_puti(int x);
+void _scc_putc(char x);
+
+int main()
+{
+    int a = 34;
+    _scc_puti(a);
+    
+    a += 35;
+    _scc_puti(a);
+
+    _scc_putc('E');
+        
+    return 0;
+}
+`, 1); //1 = moves cursor to end
+
     editor.clearSelection();
     editor.focus();
     editor.setOptions({
