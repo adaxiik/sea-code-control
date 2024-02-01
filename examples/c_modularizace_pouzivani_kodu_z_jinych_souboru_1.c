dd65ee1f4a9e4f53772e761ea9ff901287653678
adaxiik <a.jantula@seznam.cz>
2024-02-01 21:44:38 +0100

    fixed some examples

--- examples/c_modularizace_pouzivani_kodu_z_jinych_souboru_1.c
+++ examples/c_modularizace_pouzivani_kodu_z_jinych_souboru_1.c
@@ -1,16 +1,16 @@
 // https://mrlvsb.github.io/upr-skripta/c/modularizace/pouzivani_kodu_z_jinych_souboru.html, 1. runnable code block
 
-> #include <stdio.h>
-> 
-> void test() {
->   static int x = 0;
->   x += 1;
->   printf("%d\n", x);
-> }
-> 
-> int main() {
->   test();
->   test();
->   return 0;
-> }
-> 
+ #include <stdio.h>
+ 
+ void test() {
+   static int x = 0;
+   x += 1;
+   printf("%d\n", x);
+ }
+ 
+ int main() {
+   test();
+   test();
+   return 0;
+ }
+ 
